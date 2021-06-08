fe7d5f9e56de7925d27594b18bdc4c0419522a44
aravindrs44 <aravindrs@hotmail.com>
2021-06-08 13:18:04 -0700

    final

--- fundamentals/oop/usersWithBankAccounts/usersWithBankAccounts.py
+++ fundamentals/oop/usersWithBankAccounts/usersWithBankAccounts.py
@@ -5,7 +5,7 @@ class User:
         self.name = name
         self.email_address = email_address
         self.account_name = BankAccount.account_name
-        self.account = BankAccount(intrate = 0.02, balance = 0)
+        self.account = BankAccount(int_rate = 0.02, balance = 0)
 
     def display_user_balance(self):
         print(f"{self.name}: {self.account.balance}")
@@ -18,18 +18,18 @@ class User:
         self.account.withdraw(amount)
         return self
 
-    def transfer_money(self, other_user, amount):
-        self.account_balance -= amount
-        other_user.account_balance += amount
-        return self, other_user
+    # def transfer_money(self, other_user, amount):
+    #     self.account_balance -= amount
+    #     other_user.account_balance += amount
+    #     return self, other_user
 
 
 class BankAccount:
     bank_name = "Bank of India"
 
-    def __init__(self, account_name, intrate = 0.02, balance = 0):
+    def __init__(self, account_name, int_rate = 0.02, balance = 0):
         self.account_name = account_name
-        self.intrate = intrate
+        self.int_rate = int_rate
         self.balance = balance
 
     def deposit(self, amount):
@@ -44,8 +44,9 @@ class BankAccount:
             self.balance -= amount
         return self
 
+
     def yield_interest(self):
-        self.balance += (self.balance*self.intrate)
+        self.balance += (self.balance * self.int_rate)
         return self
 
     @classmethod
