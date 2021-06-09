de2a05c2c08f0ec16025641e1fbc2fab4ca4e245
aravindrs44 <aravindrs@hotmail.com>
2021-06-08 17:00:09 -0700

    initial commit

--- fundamentals/oop/dojo_pets/ninja.py
+++ fundamentals/oop/dojo_pets/ninja.py
@@ -1,23 +1,30 @@
-import pets
+from pets import Pet
 
 class Ninja:
-    def __init__(self, first_name, last_name, treats, pet_food):
+    def __init__(self, first_name, last_name, treats, pet_food, pet):
         self.first_name = first_name
         self.last_name = last_name
         self.treats = treats
         self.pet_food = pet_food
-        self.pet = Pet(name, types, tricks, health, energy)
+        self.pet = pet
 
     def walk(self):
-        self.pet.walk()
-        print(f"{self.pet.name}'s health ↑ by 5!")
+        self.pet.play()
+        print(f"{self.pet.name}'s health UP by 5!")
+        return self
     
     def feed(self):
         self.pet.eat()
-        print(f"{self.pet.name}'s Health ↑ by 5")
-        print(f"{self.pet.name}'s Energy ↑ by 10")
+        print(f"{self.pet.name}'s Health UP by 5")
+        print(f"{self.pet.name}'s Energy UP by 10")
+        return self
 
     def bathe(self):
         self.pet.noise()
+        return self
 
-senlair = Ninja("Sen", "Lair", "hot cheetos", "takis")
+
+chiaro = Pet("Chiaro", "dog", "butt wiggle")
+senlair = Ninja("Aravind", "Sripada", "hot cheetos", "takis", chiaro)
+senlair.feed().walk().bathe()
+print(chiaro.health)
--- fundamentals/oop/dojo_pets/pets.py
+++ fundamentals/oop/dojo_pets/pets.py
@@ -9,18 +9,13 @@ class Pet:
 
     def sleep(self):
         self.energy += 25
-        return self
 
     def eat(self):
         self.energy += 5
         self.health += 10
-        return self
 
     def play(self):
         self.health += 5
-        return self
 
     def noise(self):
         print("BORF")
-
-chiaro = Pet("Chiaro", "dog", "butt wiggle")
