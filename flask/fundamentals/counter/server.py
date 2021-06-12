c274558807d4a6078d1147a746c4d2a0fc248db3
aravindrs44 <aravindrs@hotmail.com>
2021-06-12 14:52:48 -0700

    initial commit

---
+++ flask/fundamentals/counter/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template, redirect, session
+app = Flask(__name__)
+app.secret_key = 'find out on the next episode of dragonball z'
+
+@app.route('/')
+def index():
+    if 'visits' in session:
+        print('visits exists!')
+        session['visits'] += 1
+    else:
+        print("key does NOT exist")
+        session['visits'] = 0
+    print(session['visits'])
+    return render_template('index.html')
+
+@app.route('/destroy_session')
+def erase():
+    session.clear()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug = True)
