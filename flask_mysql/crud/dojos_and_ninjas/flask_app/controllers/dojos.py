f74329d93acf92af58dd065098d9c0aadf29e9fa
aravindrs44 <aravindrs@hotmail.com>
2021-06-19 18:08:10 -0700

    initial commit

---
+++ flask_mysql/crud/dojos_and_ninjas/flask_app/controllers/dojos.py
@@ -0,0 +1,21 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models.dojo import Dojo
+
+@app.route('/')
+def redirectToHome():
+    return redirect('/dojos')
+
+# -----------  CREATE GET ALL USER PAGE  --------------#
+@app.route('/dojos')
+def all_dojo_page():
+    dojos_display = Dojo.get_all_dojos()
+    return render_template("dojos.html", dojos = dojos_display)
+
+# ------------  SHOW ONE USER PAGE  -------------------#
+@app.route('/dojos/<int:id>')
+def one_dojo_page(id):
+    pass
+    return render_template("dojo_show.html", dojo_id = id)
+
+# ------------  SHOW ONE USER PAGE  -------------------#
